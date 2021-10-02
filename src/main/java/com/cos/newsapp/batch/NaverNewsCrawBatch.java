9f0c526e01e22f9fdd963154bcb0d3fb9ff47de8
choi <choi.dahye424@gmail.com>
2021-10-02 10:27:34 +0900

    통신 오류 잡기(try~ catch)

--- src/main/java/com/cos/newsapp/batch/NaverNewsCrawBatch.java
+++ src/main/java/com/cos/newsapp/batch/NaverNewsCrawBatch.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.cos.newsapp.domain.NaverNews;
 import com.cos.newsapp.domain.NaverNewsRepository;
+import com.sun.tools.javap.TryBlockWriter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,39 +33,40 @@ public class NaverNewsCrawBatch {
 		for (int i = 0; i < 5; i++) {
 			String aidStr = String.format("%010d", aid);
 			String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=022&aid=" + aidStr;
-			
+
 			RestTemplate rt = new RestTemplate(); // 안드로이드 : Retrofit2(내부 쓰레드)
 
-			String html = rt.getForObject(url, String.class); // String.class 응답받은 타입
-			
-			Document doc = Jsoup.parse(html);
-			
-			Element companyElement = doc.selectFirst(".press_logo img");
-			String company = companyElement.attr("title");
-			
-			Element titleElement = doc.selectFirst("#articleTitle");
-			String title = titleElement.text();
-			
-			Element createAtElement = doc.selectFirst(".t11");
-			String createAt = createAtElement.text();
-			
-			/* 모델에서 @AllArgsConstructor, @Data 추가  
-			 * 생성자를 사용해서 데이터를 넣어준다. -> 일일이 하나씩 넣는 거 보다 편하다. 
-			 *  Builder를 사용하면 _id 안 넣어도 되고 적는 순서를 지키지 않아도 된다.
-			 */
-			
-			NaverNews nn = NaverNews.builder()
-					.company(company)
-					.title(title)
-					.createAt(createAt)
-					.build();
-			
-			newsList.add(nn);
-			
+			try {
+				String html = rt.getForObject(url, String.class); // String.class 응답받은 타입
+				Document doc = Jsoup.parse(html);
+				
+				Element companyElement = doc.selectFirst(".press_logo img");
+				String company = companyElement.attr("title");
+				
+				Element titleElement = doc.selectFirst("#articleTitle");
+				String title = titleElement.text();
+				
+				Element createAtElement = doc.selectFirst(".t11");
+				String createAt = createAtElement.text();
+				
+				/* 모델에서 @AllArgsConstructor, @Data 추가  
+				 * 생성자를 사용해서 데이터를 넣어준다. -> 일일이 하나씩 넣는 거 보다 편하다. 
+				 *  Builder를 사용하면 _id 안 넣어도 되고 적는 순서를 지키지 않아도 된다.
+				 */
+				
+				NaverNews nn = NaverNews.builder()
+						.company(company)
+						.title(title)
+						.createAt(createAt)
+						.build();
+				
+				newsList.add(nn);
+				
+			} catch (Exception e) {
+				System.out.println("통신 오류!!");
+			} // end of try~ catch
 			aid++;
-			
 		} // end of for
-		
 		naverNewsRepository.saveAll(newsList); // 벌크 컬렉터  : 한번에 저장 -> 엄청 빠르다. 
 		
 		/*
